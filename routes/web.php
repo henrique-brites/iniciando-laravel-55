2b4d38cfae385f99e9c018a77b88cc8d9f889f9c
Henrique Brites <henriquebrites@live.com>
2021-10-30 17:54:04 -0400

    feature: friendly POST routes and forms

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use \Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,23 +17,41 @@ Route::get('/', function () {
 });
 
 Route::get('/cliente', function (){
-    return "Hello World!";
+    //csrf-token
+    $csrfToken = csrf_token();
+    $html = <<< HTML
+<html>
+<body>
+    <h1>Cliente</h1>
+    <form action="/cliente/cadastrar" method="post">
+        <input type="hidden" name="_token" value="$csrfToken">
+        <input type="text" name="name" />
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
+HTML;
+    return $html;
 });
 
-Route::get('/admin//cliente', function (){
-    return "Admin - Hello World!";
+Route::post('/cliente/cadastrar', function(Request $request){
+    echo $request->get('name');
 });
 
-Route::get('/cliente-echo', function (){
-    echo "Texto com echo";
-});
-
-Route::get('/produto/{name}/{id}', function ($name, $id){
-    return "Produto $name - $id";
-});
-
-Route::get('/fornecedor/{name}/{id?}', function ($name, $id = null){
-    return "Fornecedor $name - $id";
-});
+//Route::get('/admin/cliente', function (){
+//    return "Admin - Hello World!";
+//});
+//
+//Route::get('/cliente-echo', function (){
+//    echo "Texto com echo";
+//});
+//
+//Route::get('/produto/{name}/{id}', function ($name, $id){
+//    return "Produto $name - $id";
+//});
+//
+//Route::get('/fornecedor/{name}/{id?}', function ($name, $id = null){
+//    return "Fornecedor $name - $id";
+//});
 
 //CoC Convention over Configuration
