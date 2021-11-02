2ea5caa242b8b53762f622a65273188368cbf1e5
Henrique Brites <henriquebrites@live.com>
2021-11-01 20:32:41 -0400

    feature: practice with eloquent ORM

--- app/Client.php
+++ app/Client.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model //Design Pattern - Active Record
 {
-    //
+    //protected $table = 'xpto_table';
+    //clients
+    //Category -> categories
     //ALTER TABLE cpf
 }
